f30b23d6a5076929b0aac708389d53208f747e9c
RJ Chang <loch0728@gmail.com>
2024-07-27 07:47:29 +0800

    Modify db.py

--- db.py
+++ db.py
@@ -1,4 +1,5 @@
-from pymongo import MongoClient
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
@@ -8,9 +9,9 @@ MONGO_USERNAME = os.getenv("MONGO_USERNAME")
 MONGO_PASSWORD = os.getenv("MONGO_PASSWORD")
 MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
 
-client_uri = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
+uri = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = MongoClient(client_uri)
+client = MongoClient(uri, server_api=ServerApi('1'))
 db = client.jp_syllabaries
 
 def get_kana_collection():
