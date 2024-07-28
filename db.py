db27fa79ae46db0757533c2beb264c20fe7efdbe
RJ Chang <loch0728@gmail.com>
2024-07-28 13:24:16 +0800

    Add certifi to db.py

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
+import certifi
 import os
 
 load_dotenv()
@@ -11,7 +12,7 @@ MONGO_CLUSTER = os.getenv("MONGO_CLUSTER")
 
 uri = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_CLUSTER}/?retryWrites=true&w=majority&appName=Cluster0"
 
-client = MongoClient(uri, server_api=ServerApi('1'))
+client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
 db = client.jp_syllabaries
 
 def get_kana_collection():
